05c4c8e1836017b7700453b27265644a100b161c
Ayoe <anomalous189@gmail.com>
2022-05-03 17:50:06 +0100

    amended routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Routes from './components/Routes';
 import { useState } from 'react';
+import { Switch ,Route , Redirect} from 'react-router-dom';
+import Results from './components/Results';
 
 function App() {
   const [darkTheme , setDarkTheme ] = useState(false);
@@ -10,7 +12,20 @@ function App() {
     <div className={darkTheme ? 'dark' : ''}>
       <div className='bg-gray-100 dark:bg-gray-900 dark:text-grey-200 min-h-screen'>
         <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
-        <Routes />
+        <Switch>
+        <Route exact path='/'>
+          <Redirect to='/search' />
+        </Route>
+        <Route path={'/search'} >
+           <Results />
+        </Route>
+        <Route path={'/news'} >
+           <Results />
+        </Route>
+        <Route path={'/videos'} >
+           <Results />
+        </Route>
+      </Switch>
         <Footer />
       </div>
     </div>
